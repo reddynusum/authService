b7b73fd83d50091cd03447a21bb603e282d2fd4d
KOTI REDDY NUSUM <150128464+reddynusum@users.noreply.github.com>
2026-02-24 20:17:54 +0530

    Update route.ts

--- app/api/auth/me/route.ts
+++ app/api/auth/me/route.ts
@@ -3,7 +3,7 @@ import { verifyAccessToken } from "@/lib/auth/jwt";
 const corsHeaders = {
   "Access-Control-Allow-Origin": "http://localhost:5173",
   "Access-Control-Allow-Methods": "POST, OPTIONS,GET",
-  "Access-Control-Allow-Headers": "Content-Type",
+  "Access-Control-Allow-Headers": "Content-Type,Authorization",
   "Access-Control-Allow-Credentials": "true",
 };
 
