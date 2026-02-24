fb312c5ee5ea92bba89d08cae3440909e1508a65
KOTI REDDY NUSUM <150128464+reddynusum@users.noreply.github.com>
2026-02-24 08:49:40 +0530

    Delete auth directory

--- auth/app/Test/refresh/page.tsx
+++
@@ -1,8 +0,0 @@
-'use client'
-export default function Refresh(){
-     async function refre(){
-        const data = await fetch('/api/auth/refresh',{method:'POST',credentials:'include'}).then((res)=> res.json());
-        console.log(data);
-    }
-    return(<button onClick={refre}>refresh</button>);
-}
