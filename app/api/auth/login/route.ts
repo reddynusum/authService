f62b521ce49116abfa4c71cf951375b30d1f979e
KOTI REDDY NUSUM <150128464+reddynusum@users.noreply.github.com>
2026-02-24 11:32:37 +0530

    Update route.ts

--- app/api/auth/login/route.ts
+++ app/api/auth/login/route.ts
@@ -9,8 +9,17 @@ import {
 const corsHeaders = {
   "Access-Control-Allow-Origin": "http://localhost:5173",
   "Access-Control-Allow-Methods": "POST, OPTIONS",
-  "Access-Control-Allow-Headers": "Content-Type"
+  "Access-Control-Allow-Headers": "Content-Type, Authorization",
+  "Access-Control-Allow-Credentials": "true",
 };
+
+export async function OPTIONS() {
+  return new NextResponse(null, {
+    status: 200,
+    headers: corsHeaders,
+  });
+}
+
 export async function POST(req: NextRequest) {
   try {
     await connectDB();
@@ -21,54 +30,53 @@ export async function POST(req: NextRequest) {
 
     if (!user) {
       return NextResponse.json(
-        { message: "Invalid credentials" ,status:401},
-        { headers:corsHeaders}
+        { message: "Invalid credentials" },
+        { status: 401, headers: corsHeaders }
       );
     }
 
-    const isMatch = await comparePassword(
-      password,
-      user.password
-    );
+    const isMatch = await comparePassword(password, user.password);
 
     if (!isMatch) {
       return NextResponse.json(
-        { message: "Invalid credentials",status:401 },{headers:corsHeaders}
+        { message: "Invalid credentials" },
+        { status: 401, headers: corsHeaders }
       );
     }
 
-    // generate tokens
-    const accessToken = generateAccessToken(
-        {userId:user._id.toString(),
-        email:user.email,
-      role:user.role});
-    const refreshToken = generateRefreshToken(
-        {userId:user._id.toString(),
-            email:user.email,
-          role:user.role});
+    const accessToken = generateAccessToken({
+      userId: user._id.toString(),
+      email: user.email,
+      role: user.role,
+    });
+
+    const refreshToken = generateRefreshToken({
+      userId: user._id.toString(),
+      email: user.email,
+      role: user.role,
+    });
 
     user.refreshToken = refreshToken;
     await user.save();
 
-    const response = NextResponse.json({
-      accessToken,
-      headers:corsHeaders
-    });
+    const response = NextResponse.json(
+      { accessToken },
+      { headers: corsHeaders }
+    );
 
     response.cookies.set("refreshToken", refreshToken, {
       httpOnly: true,
       secure: false,
+      sameSite: "lax",
       path: "/",
       maxAge: 7 * 24 * 60 * 60,
-      sameSite:"lax"
     });
 
     return response;
-
-  } catch (error) {
+  } catch {
     return NextResponse.json(
-      { message: "Login failed",status:500 },
-      { headers:corsHeaders }
+      { message: "Login failed" },
+      { status: 500, headers: corsHeaders }
     );
   }
 }
