45d2e4f4d367d55d18913349c0804160b82f8f05
KOTI REDDY NUSUM <150128464+reddynusum@users.noreply.github.com>
2026-03-13 00:48:14 +0530

    Update route.ts

--- app/api/auth/me/route.ts
+++ app/api/auth/me/route.ts
@@ -1,7 +1,7 @@
 import { NextResponse } from "next/server";
 import { verifyAccessToken } from "@/lib/auth/jwt";
 const corsHeaders = {
-  "Access-Control-Allow-Origin": "http://localhost:5173",
+  "Access-Control-Allow-Origin": "http://localhost:3000",
   "Access-Control-Allow-Methods": "POST, OPTIONS,GET",
   "Access-Control-Allow-Headers": "Content-Type,Authorization",
   "Access-Control-Allow-Credentials": "true",
